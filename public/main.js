e040dfa40e144e334d63ca7903cf8a0d3003ba88
wlozan00 <william.lozano20004@gmail.com>
2022-10-20 21:17:06 +0200

    Final changes

--- public/main.js
+++ public/main.js
@@ -71,6 +71,16 @@ const uvData = repeat(6, [
     0, 1,
 ]);
 
+//add the normal data coordinates
+const normalData = [
+    ...repeat(6, [ 0, 0, 1]),
+    ...repeat(6, [ 0, 0,-1]),
+    ...repeat(6, [ 0, 1, 0]),
+    ...repeat(6, [ 0,-1, 0]),
+    ...repeat(6, [ 1, 0, 0]),
+    ...repeat(6, [-1, 0, 0]),
+];
+
 //initialize buffer to send the data to the GPU
 
 const positionBuffer = gl.createBuffer();
@@ -80,6 +90,10 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 const uvBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvData), gl.STATIC_DRAW);
+
+const normalBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalData), gl.STATIC_DRAW);
 
 //load a texture image
 
@@ -118,25 +132,42 @@ gl.bindTexture(gl.TEXTURE_2D, crate);
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
 precision mediump float;
+const vec3 lightDirection = normalize(vec3(-1, 0.8, 0.1));
+const float ambient = 0.4;
+
 attribute vec3 position;   
 attribute vec2 UV;
-varying vec2 vUV;        
-uniform mat4 matrix;   //a global variable, has the same value in the shader program and in JS
+attribute vec3 normal;
+
+varying vec2 vUV;
+varying float vBrightness;
+
+uniform mat4 matrix;
+uniform mat4 normalMatrix;
 
 void main(){
+    vec3 worldNormal = (normalMatrix * vec4(normal, 1.0)).xyz;
+    float diffuse = max(0.0, dot(worldNormal, lightDirection));
+
+    vBrightness = diffuse + ambient;
     vUV = UV;
-    gl_Position = matrix * vec4(position, 1);  //matrix values manipulate the vertex positions
+    gl_Position = matrix * vec4(position, 1);
 }`);
 gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
 precision mediump float;
+
 varying vec2 vUV;
+varying float vBrightness;
+
 uniform sampler2D textureID;
 
 void main() {
-    gl_FragColor = texture2D(textureID, vUV);
+    vec4 texel = texture2D(textureID, vUV);
+    texel.xyz *= vBrightness;
+    gl_FragColor = texel;
 }`);
 gl.compileShader(fragmentShader);
 
@@ -159,6 +190,11 @@ gl.enableVertexAttribArray(uvLocation);
 gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
 gl.vertexAttribPointer(uvLocation, 2, gl.FLOAT, false, 0, 0);
 
+const normalLocation = gl.getAttribLocation(program, `normal`);
+gl.enableVertexAttribArray(normalLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, 0, 0);
+
 //draw vertecies
 
 gl.useProgram(program);
@@ -168,41 +204,48 @@ gl.enable(gl.DEPTH_TEST);
 
 const uniformLocation = {
     matrix: gl.getUniformLocation(program, 'matrix'),
+    normalMatrix: gl.getUniformLocation(program, 'normalMatrix'),
     textureID: gl.getUniformLocation(program, 'textureID'),
 };
 
-//get the texture unifomr
+//get the texture uniform
 
 gl.uniform1i(uniformLocation.textureID, 0);
 
-//create transformation matrix 
+//create matrices 
 
 const { mat4 } = glMatrix;
-const matrix = mat4.create();
+const modelMatrix = mat4.create();
 
-//input transformation (translating, scaling, rotating) data
+mat4.translate(modelMatrix, modelMatrix, [0.3, 0, -1]);
+mat4.scale(modelMatrix, modelMatrix, [0.3, 0.3, 0.3]);
 
-mat4.translate(matrix, matrix, [0.3, 0.2, -1]);
-mat4.scale(matrix, matrix, [0.3, 0.3, 0.3]);
-
-
-//create perspective projection matrix
+const viewMatrix = mat4.create();
 
 const projectionMatrix = mat4.create();
 mat4.perspective(projectionMatrix, Math.PI / 2, canvas.width / canvas.height, 1e-4, 1e4);
 
-//combine the perspective matrix and the transformatin matrix and upload it to the shader program
+const mvMatrix = mat4.create();
+const mvpMatrix = mat4.create();
 
-const resultMatrix = mat4.create();
+const normalMatrix = mat4.create();
 
 //animate rotation
 
 function animate() {
     requestAnimationFrame(animate);
-    mat4.rotateX(matrix, matrix, Math.PI / 2 / 180);
-    mat4.rotateZ(matrix, matrix, Math.PI / 2 / 180);
-    mat4.multiply(resultMatrix, projectionMatrix, matrix); //order matters
-    gl.uniformMatrix4fv(uniformLocation.matrix, false, resultMatrix);
+    mat4.rotateX(modelMatrix, modelMatrix, Math.PI / 2 / 180);
+    mat4.rotateY(modelMatrix, modelMatrix, Math.PI / 2 / 180);
+    
+    mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
+    mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);
+    
+    mat4.invert(normalMatrix, mvMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+    
+    gl.uniformMatrix4fv(uniformLocation.matrix, false, mvpMatrix);
+    gl.uniformMatrix4fv(uniformLocation.normalMatrix, false, normalMatrix);
+    
     gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
 }
 
